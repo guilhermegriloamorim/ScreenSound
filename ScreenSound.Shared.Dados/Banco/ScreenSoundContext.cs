02b8b03d7f3faaba9ebd6e6e0212ffce507ccb02
guilh <guilh@D3LLG15GUI>
2024-04-28 21:29:03 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using ScreenSound.Shared.Dados.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
+namespace ScreenSound.Banco;
+public class ScreenSoundContext: IdentityDbContext<PessoaComAcesso, PerfilComAcesso, int>
+{
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+
+    private string connectionString = "Data Source=localhost;Initial Catalog=ScreenSoundDB;User Id=sa;Password=Zaqwsx2001@;TrustServerCertificate=True";
+
+    public ScreenSoundContext()
+    {
+        
+    }
+    public ScreenSoundContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            return;
+        }
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Musica>()
+            .HasMany(c => c.Generos)
+            .WithMany(c => c.Musicas);
+    }
+
+}
